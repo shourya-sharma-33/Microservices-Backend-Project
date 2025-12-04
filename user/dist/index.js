bbcf289c31b3d93ad9310526be9cfad44551db4b
shourya-sharma-33 <shourya-ml-dl-artificial-intelligence@proton.me>
2025-12-05 01:15:06 +0530

    User Setups : Some More Setup किया यूजर  मे

--- user/dist/index.js
+++ user/dist/index.js
@@ -1,10 +1,7 @@
 import express from "express";
-
 const application = express();
-
 const PORT = 5000;
-
 application.listen(PORT, () => {
     console.log("Server Running on 5000");
-    
-})
+});
+//# sourceMappingURL=index.js.map
