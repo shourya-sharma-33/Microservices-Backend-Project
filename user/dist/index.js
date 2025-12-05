956e07605c700b079e45f9bd38ca9e4a12fef61a
shourya-sharma-33 <shourya-ml-dl-artificial-intelligence@proton.me>
2025-12-05 22:53:06 +0530

    Redis Setup and All

--- user/dist/index.js
+++ user/dist/index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import { createClient } from "redis";
+import userRoutes from "./routes/user.js";
 const application = express();
+dotenv.config();
+connectDb();
+export const redisClient = createClient({
+    url: process.env.REDIS_URL,
+});
+redisClient.connect().then(() => console.log("connect to redis")).catch(console.error);
 const PORT = 5000;
+application.use("api/v1", userRoutes);
 application.listen(PORT, () => {
     console.log("Server Running on 5000");
 });
